e864c40ef7302c94d2569defa7ae33209ee7b679
mrcool7387 <146818600+mrcool7387@users.noreply.github.com>
2025-11-10 14:57:56 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from _template import LOGGER, TMPDIR, TMPFILE
+
+
+
+def main() -> None:
+    pass
+
+if __name__ == "__main__":
+    LOGGER.info("Running MainFile")
+    LOGGER.debug(__name__)
+    LOGGER.debug(__file__)
+    main()
