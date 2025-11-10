b6bbd3012ad58c6eac6115dfc9dcf06739b0eff0
Alexander Schwarz <alexander.schwarz@schueler.htr-office.de>
2025-11-10 15:01:04 +0100

    Initial Commit

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-from _template import LOGGER, TMPDIR, TMPFILE
+def main():
+    print("Hello from ogsafkscreen!")
 
-
-
-def main() -> None:
-    pass
 
 if __name__ == "__main__":
-    LOGGER.info("Running MainFile")
-    LOGGER.debug(__name__)
-    LOGGER.debug(__file__)
     main()
